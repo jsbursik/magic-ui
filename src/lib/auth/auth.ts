8947487313bf8799d4f63a564aaea633d174039a
Jordan Bursik <jsbursik@gmail.com>
2025-10-14 15:13:29 -0500

    auth was acting up, rewrite for a different pattern

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,15 +1,11 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import * as schema from "$lib/db/schema.js";
 
 import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
-import type { Sql } from "postgres";
 
-export function createAuth(db: PostgresJsDatabase<Record<string, never>> & { $client: Sql<{}> }) {
-  const { $client, ...cleanDb } = db as any;
-
+export function createAuth(db: PostgresJsDatabase, schema: Record<string, any>) {
   return betterAuth({
-    database: drizzleAdapter(cleanDb, {
+    database: drizzleAdapter(db, {
       provider: "pg",
       schema,
     }),
--- templates/lib/server/auth.ts
+++ templates/lib/server/auth.ts
@@ -1,4 +1,5 @@
 import { createAuth } from "@jsbursik/magic-ui/server";
 import { db } from "./db.js";
+import * as schema from "$lib/db/schema.js";
 
-export const auth = createAuth(db);
+export const auth = createAuth(db, schema);
