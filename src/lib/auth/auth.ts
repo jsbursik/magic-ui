e8b007dfba38e8fb993979097f8b453a5b047ce4
Jordan Bursik <jsbursik@gmail.com>
2025-10-14 14:33:10 -0500

    fix typing

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -3,10 +3,13 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from "$lib/db/schema.js";
 
 import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import type { Sql } from "postgres";
 
-export function createAuth(db: PostgresJsDatabase) {
+export function createAuth(db: PostgresJsDatabase<Record<string, never>> & { $client: Sql<{}> }) {
+  const { $client, ...cleanDb } = db as any;
+
   return betterAuth({
-    database: drizzleAdapter(db, {
+    database: drizzleAdapter(cleanDb, {
       provider: "pg",
       schema,
     }),
