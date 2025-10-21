d8d5c1c8bb591b640ca8a79bb09e04f5918ef132
Jordan Bursik <jsbursik@gmail.com>
2025-10-20 23:44:41 -0500

    Added 2 layouts to css - need to refactor for semantic reasons

--- src/routes/test/test2/+page.svelte
+++ src/routes/test/test2/+page.svelte
@@ -1,3 +1,14 @@
-<div class="center center-text">
-  <h2>Test Page 2</h2>
+<script lang="ts">
+  import { IconHome } from "@tabler/icons-svelte";
+</script>
+
+<div class="side-col">
+  <div class="side-bar">
+    <IconHome />
+  </div>
+  <div class="side-main">
+    <div class="card center-text">
+      <h2>Test Page with <code>.side-col</code> layout</h2>
+    </div>
+  </div>
 </div>
