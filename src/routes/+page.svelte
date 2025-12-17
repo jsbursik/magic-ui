452c25992dc04817417138619eb7e13057b7ac3b
Jordan Bursik <jsbursik@gmail.com>
2025-12-17 14:03:38 -0600

    removed unnecessary import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,3 @@
-<script>
-  import Slider from "$lib/components/form-components/slider.svelte";
-</script>
-
 <main class="center center-text">
   <h1>Welcome to SvelteKit</h1>
   <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
