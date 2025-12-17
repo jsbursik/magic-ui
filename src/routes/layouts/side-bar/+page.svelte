aa22d7866045050e38d62d36daae1bd5814a0f75
Jordan Bursik <jsbursik@gmail.com>
2025-12-17 12:14:45 -0600

    re-did layout pages

---
+++ src/routes/layouts/side-bar/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { IconHome } from "@tabler/icons-svelte";
+</script>
+
+<aside class="side-bar">
+  <IconHome />
+</aside>
+<main class="full-width">
+  <div class="card center-text">
+    <h2>Test Page with <code>.side-bar</code> layout</h2>
+    <p>
+      To achieve this, you have one <code>aside</code> and one <code>main</code> element. Use the <code>side-bar</code> class on the <code>aside</code> element
+      to make the side bar.
+      <br /><br />
+      If you would like the side bar to appear on the right instead, use the <code>right</code> class on the <code>aside</code> element.
+      <br /><br />
+      The side-bar has a width of <code>fit-content</code> so it will automatically adjust to whatever size you wish.
+      <br /><br />
+      This page is also using the <code>full-width</code> class on the <code>main</code> element to use the full width of the screen to allow for more space.
+    </p>
+  </div>
+</main>
