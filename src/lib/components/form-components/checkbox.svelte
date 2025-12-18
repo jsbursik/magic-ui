8e80f52e9f3ae30b1e13adea002661e4d0ab815f
Jordan Bursik <jsbursik@gmail.com>
2025-12-18 16:45:20 -0600

    designed upload and checkbox, need functional portions

---
+++ src/lib/components/form-components/checkbox.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  let { id, name = id, children } = $props();
+</script>
+
+<div style="display: inline-flex; line-height: 1.1rem">
+  <label class="cb-container">
+    <input type="checkbox" {id} {name} checked />
+    <span class="checkmark"></span>
+  </label>
+  <label for={id}>{@render children()}</label>
+</div>
+
+<style>
+  /* Customize the label (the cb-container) */
+  .cb-container {
+    display: block;
+    position: relative;
+    padding-left: 28px;
+    cursor: pointer;
+    font-size: 0.9rem;
+    font-weight: bold;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  /* Hide the browser's default checkbox */
+  .cb-container input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  /* Create a custom checkbox */
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 1.1rem;
+    width: 1.175rem;
+    border-radius: 0.4rem;
+    background-color: var(--color-btn);
+  }
+
+  /* On mouse-over, add a grey background color */
+  .cb-container:hover input ~ .checkmark {
+    background-color: var(--color-btn-hover);
+  }
+
+  /* When the checkbox is checked, add a blue background */
+  .cb-container input:checked ~ .checkmark {
+    background-color: var(--color-primary);
+  }
+
+  /* Create the checkmark/indicator (hidden when not checked) */
+  .checkmark:after {
+    content: "✓";
+    position: absolute;
+    display: none;
+    color: white;
+  }
+
+  /* Show the checkmark when checked */
+  .cb-container input:checked ~ .checkmark:after {
+    display: block;
+  }
+
+  /* Style the checkmark/indicator */
+  .cb-container .checkmark:after {
+    left: 4px;
+    top: 0px;
+  }
+</style>
---
+++ src/lib/components/form-components/upload.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  let { id, name = id, accept = undefined, onchange = undefined } = $props();
+</script>
+
+<label for={id} class="custom-file-upload">
+  <span>Upload your file here</span>
+</label>
+<input type="file" {id} {name} {accept} {onchange} multiple />
+
+<style>
+  input[type="file"] {
+    display: none;
+  }
+
+  .custom-file-upload {
+    width: 100%;
+    height: 6rem;
+
+    place-content: center;
+    text-align: center;
+
+    border: 2px dashed var(--color-btn);
+    border-radius: var(--radius);
+    background-color: var(--color-input-bg);
+
+    cursor: pointer;
+
+    margin-bottom: unset;
+  }
+</style>
